b26774df9141e3278859855e5e0e1825a4365c54
safak <safakciplak1990@gmail.com>
2020-01-09 21:48:42 +0300

    published unpublished factory states

--- database/factories/ConcertFactory.php
+++ database/factories/ConcertFactory.php
@@ -20,3 +20,16 @@ $factory->define(Concert::class, function (Faker $faker) {
         'additional_information' => 'Some sample additional information.',
     ];
 });
+
+
+$factory->state(Concert::class, 'published', function(Faker $faker){
+    return [
+      'published_at' => Carbon::parse('-1 week')
+    ];
+});
+
+$factory->state(Concert::class, 'unpublished', function(Faker $faker){
+    return [
+        'published_at' => null
+    ];
+});
--- tests/Feature/ViewConcertListingTest.php
+++ tests/Feature/ViewConcertListingTest.php
@@ -14,7 +14,8 @@ class ViewConcertListingTest extends TestCase
     /** @test */
     public function user_can_view_a_published_concert_listing()
     {
-        $concert = Concert::create([
+
+        $concert = factory(Concert::class)->states('published')->create([
             'title' => 'The Red Chord',
             'subtitle' => 'with Animosity and Lethargy',
             'date' => Carbon::parse('December 13, 2016 8:00pm'),
@@ -25,7 +26,6 @@ class ViewConcertListingTest extends TestCase
             'state' => 'ON',
             'zip' => '17916',
             'additional_information' => 'For tickets, call (555) 555-5555.',
-            'published_at' => Carbon::parse('-1 week'),
         ]);
 
         $response = $this->get('/concerts/'. $concert->id);
@@ -45,9 +45,7 @@ class ViewConcertListingTest extends TestCase
     /** @test */
     public function user_cannot_view_unpublished_concert_listings()
     {
-        $concert = factory(Concert::class)->create([
-            'published_at' => null,
-        ]);
+        $concert = factory(Concert::class)->states('unpublished')->create();
 
         $response = $this->get('/concerts/'. $concert->id);
 
