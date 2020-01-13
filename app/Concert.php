e41eebda2577657fcb5c62a499599cd11a66abea
safak <safakciplak1990@gmail.com>
2020-01-13 17:03:57 +0300

    precomputing the order amount

--- app/Concert.php
+++ app/Concert.php
@@ -64,7 +64,7 @@ class Concert extends Model
 
     public function createOrder($email, $tickets)
     {
-       return Order::forTickets($tickets, $email);
+       return Order::forTickets($tickets, $email, $tickets->sum('price'));
     }
 
     public function addTickets($quantity)
