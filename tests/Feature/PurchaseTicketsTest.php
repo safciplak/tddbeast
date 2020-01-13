5c4c74cc0e7c5b96a48502a59725bd1f353d6104
safak <safakciplak1990@gmail.com>
2020-01-13 03:52:44 +0300

    add email validation test

--- tests/Feature/PurchaseTicketsTest.php
+++ tests/Feature/PurchaseTicketsTest.php
@@ -8,30 +8,37 @@ use App\Concert;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Tests\TestCase;
 
+/**
+ * @property FakePaymentGateway paymentGateway
+ */
 class PurchaseTicketsTest extends TestCase
 {
     use DatabaseMigrations;
 
+    /**
+     *
+     */
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->paymentGateway = new FakePaymentGateway;
+        $this->app->instance(PaymentGateway::class, $this->paymentGateway);
+    }
+
     /** @test */
     public function customer_can_purchase_concert_tickets()
     {
-
-        $paymentGateway = new FakePaymentGateway;
-
-        $this->app->instance(PaymentGateway::class, $paymentGateway);
-
         $concert = factory(Concert::class)->create(['ticket_price' => 3250]);
 
         $response = $this->json('POST', "/concerts/{$concert->id}/orders", [
             'email' => 'john@example.com',
             'ticket_quantity' => 3,
-            'payment_token' => $paymentGateway->getValidTestToken(),
+            'payment_token' => $this->paymentGateway->getValidTestToken(),
         ]);
-
 
         $response->assertStatus(201);
 
-        $this->assertEquals(9750, $paymentGateway->totalCharges());
+        $this->assertEquals(9750, $this->paymentGateway->totalCharges());
 
         $order = $concert->orders()->where('email', 'john@example.com')->first();
         $this->assertNotNull($order);
@@ -40,4 +47,17 @@ class PurchaseTicketsTest extends TestCase
         $this->assertEquals(3, $order->tickets()->count());
     }
 
+    /** @test */
+    public function email_is_required_to_purchase_tickets()
+    {
+        $concert = factory(Concert::class)->create();
+
+        $response = $this->json('POST', "/concerts/{$concert->id}/orders", [
+            'ticket_quantity' => 3,
+            'payment_token' => $this->paymentGateway->getValidTestToken(),
+        ]);
+
+        $response->assertStatus(422);
+        $this->assertArrayHasKey('errors', $response->decodeResponseJson());
+    }
 }
