c1e2cdc8e360b4a578229a39c0952ed2a8d435ce
safak <safakciplak1990@gmail.com>
2020-01-14 14:54:13 +0300

    avoiding service classes with method injection

--- app/Http/Controllers/ConcertOrdersController.php
+++ app/Http/Controllers/ConcertOrdersController.php
@@ -37,15 +37,8 @@ class ConcertOrdersController extends Controller
         ]);
 
         try {
-            // Find some tickets
             $reservation = $concert->reserveTickets(request('ticket_quantity'), request('email'));
-
-            // Charge the customer for the tickets
-            $this->paymentGateway->charge($reservation->totalCost(), request('payment_token'));
-
-            // Create an order for those tickets
-            $order = $reservation->complete();
-
+            $order = $reservation->complete($this->paymentGateway, request('payment_token'));
 
             return response()->json($order, 201);
         } catch (PaymentFailedException $e) {
